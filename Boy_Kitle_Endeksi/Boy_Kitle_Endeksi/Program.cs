f9b5edd40139b7681e112d7252b0ce0955792efd
omrdnrr <87868371+omrdnrr@users.noreply.github.com>
2022-12-19 13:51:39 +0300

    Add files via upload

---
+++ Boy_Kitle_Endeksi/Boy_Kitle_Endeksi/Program.cs
@@ -0,0 +1,26 @@
+﻿{
+    Console.WriteLine("Kilonuzu giriniz");
+    int kilo = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Boyunuzu giriniz");
+    double boy = Convert.ToDouble(Console.ReadLine());
+
+    double beden_kitle_endeksi = kilo / (boy * boy); 
+
+    if (beden_kitle_endeksi < 18)
+    {
+        Console.WriteLine("Beden kitle endeksiniz düşük zayıfsınız...");
+        Console.ReadLine();
+    }
+
+    else if (beden_kitle_endeksi > 18 && beden_kitle_endeksi < 25)
+    {
+        Console.WriteLine("Beden kitle endeksiniz normal...");
+        Console.ReadLine();
+    }
+
+    else if (beden_kitle_endeksi > 25)
+    {
+        Console.WriteLine("Beden kitle endeksiniz yüksek obezsiniz...");
+        Console.ReadLine();
+    }
+}
