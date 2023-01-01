cb648085a998a574a694339aa25bbfcdcc5ab831
omrdnrr <87868371+omrdnrr@users.noreply.github.com>
2023-01-02 01:09:50 +0300

    Add files via upload

---
+++ Girilensayikadartoplama/Girilensayikadartoplama/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+    int a, c, d = 0, f, e;
+    Console.WriteLine("Kaç Adet Sayi Girmek İstediğinizi Yazin");
+    a = Convert.ToInt32(Console.ReadLine());
+    f = a;
+    do
+    {
+        Console.WriteLine("Bir Sayi giriniz" + "(" + a + ")");
+        c = Convert.ToInt32(Console.ReadLine());
+        a--;
+        d += c;
+    } while (a > 0);
+    e = d / f;
+    Console.WriteLine("Girdiğiniz Sayıların Toplamı: " + d);
+    Console.WriteLine("Girdiğiniz Sayıların Ortalaması: " + e);
+
+}
