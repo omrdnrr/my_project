d1c449b7a569abbc57a1f3bbfed38fd91d52fc41
omrdnrr <87868371+omrdnrr@users.noreply.github.com>
2023-01-02 00:53:14 +0300

    Add files via upload

---
+++ odev_girilen_sayi_ile_1_arasindaki_cift_ve_tek_sayilarin_toplami/odev_girilen_sayi_ile_1_arasindaki_cift_ve_tek_sayilarin_toplami/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 0, b = 0;
+Console.Write("1. sayıyı giriniz:");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("2. sayıyı giriniz:");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    int c = a;
+    a = b;
+    b = c;
+}
+
+int ciftToplam = 0, tekToplam = 0;
+for (int i = a; i <= b; i++)
+{
+    if (i % 2 == 0)
+        ciftToplam += i;
+    else
+        tekToplam += i;
+}
+Console.WriteLine("Çift Sayıların Toplamı={0}\nTek Sayıların Toplamı={1}", ciftToplam, tekToplam);
+Console.ReadKey();
